64c8617a0b3a0729118b9b3e2a02d96c4fa4a8e4
ruben021000 <rubenmarinsamper@gmail.com>
2023-03-22 11:12:22 +0100

    commit_4

--- functions.py
+++ functions.py
@@ -4,10 +4,13 @@ url = "C://cosas uni//3Tercero//ExamenPython//Examen2023Python//winequality.csv"
 
 def read_data(fichero):
     with open(fichero, 'r') as file:
+        dic = {}
+        contador = 0
+        contador_lineas = 0
         reader = csv.reader(file)
         for row in reader:
-            dic
-        reader = csv.reader(file, delimiter = "\t")
+                dic = {row[0]:row} 
+    return dic
 
 print(read_data(url))
 
